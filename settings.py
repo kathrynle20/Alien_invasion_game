3d224ee429699519f1d158fafe7f65c2353d61f0
KittyKat <kathrynlle9@gmail.com>
2020-07-07 16:56:37 -0400

    debugged ship movements

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
 class Settings():
-    
-    def __init__(self):
+    """A class to store all settings for Alien Invasion."""
 
-        self.screen_width=1200
-        self.screen_height=800
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 650
         self.bg_color = (230,230,230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
